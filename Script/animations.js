ea223466bf12650212472a67356f9cf9646ac0cd
cromeros <cromerosanchez@wsu.edu'clear>
2023-10-06 08:05:14 -0700

    Stylized navbar to create effect of glow and underline when hovered. Added padding to navbar. Created columns to showcase projects in main_website.html. Created another transition for projects from right to left and fixed animations.js to do so. Added better images for Real Stats. Fixed description a bit for it too. Added a whole new section to the main_website.html where I can better showcase my personal projects.

--- Script/animations.js
+++ Script/animations.js
@@ -12,3 +12,6 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer.observe(el));
