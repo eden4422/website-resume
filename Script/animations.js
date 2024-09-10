30915327c1d7e476d9473a0d6ad132575c7d11c9
cromeros <cromerosanchez@wsu.edu'clear>
2024-09-10 02:44:13 -0700

    better styling with prettier and some changes to CSS

--- Script/animations.js
+++ Script/animations.js
@@ -1,17 +1,17 @@
 // Creates sliding anymation for text and logos
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-const hiddenElements2 = document.querySelectorAll('.hidden2');
+const hiddenElements2 = document.querySelectorAll(".hidden2");
 hiddenElements2.forEach((el) => observer.observe(el));
--- Script/scripts.js
+++ Script/scripts.js
@@ -1,23 +1,19 @@
 function openStatsRepo() {
-
-    let url = 'https://github.com/eden4422/Personal-Well-Being-Stats/tree/main';
-    window.open(url, '_blank');
+  let url = "https://github.com/eden4422/Personal-Well-Being-Stats/tree/main";
+  window.open(url, "_blank");
 }
 
 function openChessRepo() {
-
-    let url = 'https://github.com/eden4422/Chess-Game';
-    window.open(url, '_blank');
+  let url = "https://github.com/eden4422/Chess-Game";
+  window.open(url, "_blank");
 }
 
 function openWebsiteRepo() {
-
-    let url = 'https://github.com/eden4422/website-resume';
-    window.open(url, '_blank');
+  let url = "https://github.com/eden4422/website-resume";
+  window.open(url, "_blank");
 }
 
 function openDiscordBotRepo() {
-
-    let url = 'https://github.com/eden4422/Python-Discord-Bot';
-    window.open(url, '_blank');
+  let url = "https://github.com/eden4422/Python-Discord-Bot";
+  window.open(url, "_blank");
 }
