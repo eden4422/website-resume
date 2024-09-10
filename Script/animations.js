99a19a45fd42b7e008c0f83a4766034b43551b5c
cromeros <cromerosanchez@wsu.edu'clear>
2024-09-10 06:20:15 -0700

    fixed current version to work properly

--- Script/animations.js
+++ Script/animations.js
@@ -1,13 +1,19 @@
 // Creates sliding anymation for text and logos
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     } else {
       entry.target.classList.remove("show");
     }
   });
+});
+
+const logos = document.querySelectorAll(".logo");
+
+logos.forEach((logo, index) => {
+  const delay = 100 * index;
+  logo.style.transitionDelay = `${delay}ms`;
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
